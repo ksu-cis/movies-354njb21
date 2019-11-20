5e1f649034b8d621e779e575ae6033e7c20efac5
Nathan Brown <njbrown@ksu.edu>
2019-11-20 14:51:51 -0600

    Static

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,21 +9,49 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-        MovieDatabase movieDatabase = new MovieDatabase();
+        
         public List<Movie> Movies;
 
+        [BindProperty]
+        public string search { get; set; }
+
+        [BindProperty]
+        public List<string> mpaa { get; set; } = new List<string>();
+
+        [BindProperty]
+        public float? minIMDB { get; set; }
+
+        [BindProperty]
+        public float? maxIMDB { get; set; }
+
+        
         public void OnGet()
         {
-            Movies = movieDatabase.All;
+            Movies = MovieDatabase.All;
         }
 
-        public void OnPost(string search, List<string> rating)
+        public void OnPost()
         {
+            Movies = MovieDatabase.All; 
+            if(search != null)
+            {
+                Movies = MovieDatabase.Search(Movies, search);
+            }
            
+            if(mpaa.Count != 0)
+            {
+                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+            }
+
+            if (minIMDB != null)
+            {
+                Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+            }
             
-                Movies = movieDatabase.SearchAndFilter(search, rating);
-            
-            
+            if(maxIMDB != null)
+            {
+                Movies = MovieDatabase.FilterByMaxIMDB(Movies, (float)maxIMDB);
+            }
         }
     }
 }
